65b5b874f1e89dfb2475b3df0af4967db6426c28
Katerina Borisova <katerinaborisova@MacBook-Air-Katerina.local>
2025-09-14 13:10:27 +0300

    Remove node_modules from git

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const http = require('http');
 const fs = require('fs');
-const { error } = require('console');
+//const { error } = require('console');
+const debug = require('debug');
+const log = debug('server');
+
 const SERVER_PORT = 3000;
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    console.log('req urls', url);
+    log('req urls', url);
     
     const fileName = url === '/' ? '/index.html' : url;
     
-    fs.readFile(`${__dirname}/..${fileName}`, (err, file) => {
+    fs.readFile(`${__dirname}/../public${fileName}`, (err, file) => {
         //console.log('async after reading');
         if(err){
-            console.log('some error');
+            log('some error');
             res.write('smth required');
             res.end();
             return;
